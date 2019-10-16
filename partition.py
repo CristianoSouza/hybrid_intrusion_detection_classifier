642e1159cbead274f256c531f2bb45a871c7a0d9
Your Name <you@example.com>
2019-10-16 11:34:59 -0300

    partition update

--- partition.py
+++ partition.py
@@ -19,12 +19,13 @@ from evaluate_module import EvaluateModule
 
 dts = DataSet()
 
+print("Iniciando particionamento....")
 #pasta pra salvar
-dts.setFilePath("../../Bases/MachineLearningCVE/teste/")
-#dts.setFilePath("../../Bases/MachineLearningCVE/DoS/")
+#dts.setFilePath("../../Bases/MachineLearningCVE/teste/")
+dts.setFilePath("../../Bases/MachineLearningCVE/DoS/")
 
 #caminho e nome do arquivo
-dts.setFileName("../../Bases/MachineLearningCVE/teste_ddos_BINARY.csv")
-#dts.setFileName("../../Bases/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos_BINARY.pcap_ISCX.csv")
+#dts.setFileName("../../Bases/MachineLearningCVE/teste_ddos_BINARY.csv")
+dts.setFileName("../../Bases/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos_BINARY.pcap_ISCX.csv")
 
 dts.loadData(10)
