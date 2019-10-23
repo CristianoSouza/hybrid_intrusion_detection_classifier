562d6c46716519de870338ce8e4e7242d6dd7e88
Your Name <you@example.com>
2019-10-23 11:29:30 -0300

    aa

---
+++ partition56.py
@@ -0,0 +1,27 @@
+import os
+from dataSet import DataSet
+
+
+arquivo = open( "saidaaa.csv", 'w') 
+arquivo.write("CHAMOU PARTITION") 
+arquivo.close()
+
+dts = DataSet()
+
+print("Iniciando particionamento....")
+arquivo = open( "saidaaa.csv", 'a')
+arquivo.write("Iniciando particionamento....A") 
+arquivo.close()
+#pasta pra salvar
+
+#dts.setFilePath("../../Bases/MachineLearningCVE/teste/")
+dts.setFilePath("../../Bases/MachineLearningCVE/DoS_56att2/")
+
+#caminho e nome do arquivo
+#dts.setFileName("../../Bases/MachineLearningCVE/teste_ddos_BINARY.csv")
+dts.setFileName("../../Bases/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos_BINARY_56att.pcap_ISCX.csv")
+print("chamando load")
+arquivo = open( "saidaaa.csv", 'a')
+arquivo.write("chamando load...A")
+arquivo.close()
+dts.loadData(10)
